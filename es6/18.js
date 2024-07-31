1795a0a9d13230945a38a7f995643a49faf935a2
yungkhann <ertaev.19102007@gmail.com>
2024-07-31 14:14:28 +0500

    8

---
+++ es6/18.js
@@ -0,0 +1,13 @@
+const createPerson = (name, age, gender) => {
+  'use strict';
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender,
+  };
+  // Only change code above this line
+};
+
+const ualikhan = createPerson('Rafael', 27, 'Male');
+console.log(ualikhan);
---
+++ es6/19.js
@@ -0,0 +1,9 @@
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  },
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
